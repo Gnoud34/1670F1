05955e89dd5a9c06f9ccfa3129a1637e10b9cdf3
reversefall <nphutien.1704@gmail.com>
2024-10-03 01:32:12 +0700

    Add Identity

--- 1670F1/1670F1/obj/Debug/netcoreapp3.1/1670F1.AssemblyInfo.cs
+++ 1670F1/1670F1/obj/Debug/netcoreapp3.1/1670F1.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1670F1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36f75b6014127e2971c6571a968f44d2e55db14e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eafee8c3021bd8d28871e3568ee1ed3294d6c8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("1670F1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1670F1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
